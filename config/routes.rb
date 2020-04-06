f45fb2265fe6749a32556b3d7fdc0277942e6f76
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2020-04-06 18:40:45 +0200

    conditional navbar, custom logo, config routes, test integration layouts

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   end
   resources :cities, only: :show
   resources :profiles, only: :show
-  get 'contact', to: 'static_pages#contact'
-  get 'team', to: 'static_pages#team'
+  get 'contact',       to: 'static_pages#contact'
+  get 'team',          to: 'static_pages#team'
   get 'welcome/:name', to: 'static_pages#welcome'
-  root to: 'gossips#index'
+  root                     'gossips#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+  	get gossips_path
+  	assert_select "a[href=?]", root_path, count: 2
+  	assert_select "a[href=?]", team_path
+  	assert_select "a[href=?]", contact_path
+  	assert_select "a[href=?]", new_session_path
+  end
+
+	test "the team page" do
+  	get "/team"
+  	assert_select 'h1', "The team"
+	end
+end
